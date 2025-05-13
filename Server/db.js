3ea2299a67d30b3a89ba9bddf2542390da7f05d2
Betty <827941041@qq.com>
2025-05-13 11:26:03 -0400

    external sql

--- Server/db.js
+++ Server/db.js
@@ -1,12 +1,20 @@
 const { Pool } = require('pg');
 
-// 创建连接池
+// local pool
+// const pool = new Pool({
+//   user: 'postgres', 
+//   host: 'localhost',  
+//   database: 'postgres',  
+//   password: '12345',  
+//   port: 5432, 
+// });
+
+// external pool
 const pool = new Pool({
-  user: 'postgres', 
-  host: 'localhost',  
-  database: 'postgres',  
-  password: '12345',  
-  port: 5432, 
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = pool;
